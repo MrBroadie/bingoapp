4bb25ee877980541b41e3f3cc16536eb18b24f09
Caolan <caolanhamilton75@gmail.com>
2022-12-20 10:40:25 +0000

    Add animations back for winner

--- bingo-app-fe/tailwind.config.js
+++ bingo-app-fe/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    extend: {
+      animation: {
+        "ping-short": "ping 2s ease-in-out 1",
+        "spin-short": "spin 1s ease-in-out 1",
+      },
+    },
     plugins: [],
   },
 };
